7c78a897df6d09e9951fc2f540909046cacaf4a7
Junaid <Junaid.abdulazeez10@gmail.com>
2026-08-18 15:22:21 +0200

    created the habit class

---
+++ habit.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+class Habit:
+  def __init__(self, name: str, description:str , frequency: str):
+    self.name = name
+    self.description = description
+    self.frequency = frequency
+    self.created_at: datetime = datetime.now()
+    self.completed_dates: list[datetime] = []
+    
---
+++ main.py
@@ -0,0 +1,3 @@
+from habit import Habit
+habit = Habit("Drink Water", "Do it man", 5)
+print(habit.completed_dates)
