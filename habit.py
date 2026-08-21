5c0539a4c6f3ebf6dd71bacb4268f6a60e63a658
Junaid <Junaid.abdulazeez10@gmail.com>
2026-08-22 01:51:06 +0200

    modified complete() method to check if a today's date is already in the array

--- habit.py
+++ habit.py
@@ -8,4 +8,11 @@ class Habit:
     self.completed_dates: list[datetime] = []
 
   def complete(self):
+    today_date = datetime.now().date()
+    
+    for completed_date in self.completed_dates:
+      if(completed_date.date() == today_date):
+        return
+      
     self.completed_dates.append(datetime.now())
+  
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from habit import Habit
-from habit_tracker import HabitTracker
 
-daily_habit = Habit("Exercise", "do sport", "daily")
-weekly_habit = Habit("Clean room", "clean your room man", "weekly")
+habit = Habit("Exercise", "do sport", "daily")
 
+print("Before:", habit.completed_dates)
 
+habit.complete()
 
-tracker = HabitTracker()
+print("After first:", habit.completed_dates)
 
-tracker.complete_habit(daily_habit)
+habit.complete()
 
-print(daily_habit.completed_dates)
-
-
+print("After second:", habit.completed_dates)
